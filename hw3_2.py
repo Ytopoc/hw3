3a9205a867c0e2184d4cbd11ec13420d2aed1e1b
Ytopoc <43822389+Ytopoc@users.noreply.github.com>
2024-03-12 12:04:38 +0200

    Update hw3_2.py

--- hw3_2.py
+++ hw3_2.py
@@ -3,7 +3,7 @@ def get_numbers_ticket(umin, umax, quantity):
     lottery_set =set()
     if umin < 1 or umin >= umax or umax > 1000 or quantity < 1 or quantity > umax:
         print( "Не вірні дані" ) 
-        return None
+        return list(lottery_set) 
     else:
         while not quantity == len(lottery_set):
             lottery_set.add(random.randint(umin , umax))
